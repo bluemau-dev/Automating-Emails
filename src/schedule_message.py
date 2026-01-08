b9145f5b63eeded5f0b4d44cdc06ed409d0a135f
Mauricio Plaza-Cabello <bluemau.dev@gmail.com>
2026-01-07 22:56:29 -0600

    Testing and learning more about time module. Wrote notes and learned about Binding.

--- src/schedule_message.py
+++ src/schedule_message.py
@@ -1,5 +1,15 @@
 import time
 
+# Current Time
 curr = time.time()
+
+# Print Starting Point Eloch Time in Seconds
 print(curr)
+
+# Tuple of Current Time
 print(time.gmtime(curr))
+
+
+# Binding new Module Object
+t = time
+print(t.time())
